875f007a7cdbcaad313cea216046bf3932681c35
4ever2 <3417013+4ever2@users.noreply.github.com>
2026-07-08 21:38:51 +0200

    Disable unboxing pass by default in all backends

--- theories/backends/OCamlBackend.v
+++ theories/backends/OCamlBackend.v
@@ -30,7 +30,7 @@ Definition ocaml_phases := {|
   implement_lazy_c := Compatible false;
   cofix_to_laxy_c  := Compatible false;
   betared_c        := Compatible false;
-  unboxing_c       := Compatible true;
+  unboxing_c       := Compatible false;
   dearg_ctors_c    := Compatible true;
   dearg_consts_c   := Compatible true;
 |}.
